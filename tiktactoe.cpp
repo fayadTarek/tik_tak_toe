05daadc86811113aeacd2ef89980204c0e90f22e
fayadTarek <140421363+fayadTarek@users.noreply.github.com>
2023-08-04 16:23:49 +0300

    addet a joue fonction

--- tiktactoe.cpp
+++ tiktactoe.cpp
@@ -4,7 +4,9 @@ using namespace std;
 enum joueur {X, O, vide};
 
 typedef array<array<joueur ,3>,3> tik;
-
+tik initialise ( tik &ar);
+void print(tik ar);
+void joue(bool jouer , tik &ar);
 
 tik initialise ( tik &ar){
     for (size_t i = 0; i <= ar.size(); i++)
@@ -41,16 +43,41 @@ void print(tik ar){
     {
         cout << i << "   ";
     }
-    
+    cout << endl << endl;
     
 }
 
+void joue(bool jouer , tik &ar){
+    joueur Joueur;
+
+    if(jouer == true){
+        Joueur = X;
+    }else{
+        Joueur = O;
+    }
+
+    size_t colonne;
+    size_t ligne;
+
+    do
+    {
+        cout << ">entrez le colonne :";
+        cin >> colonne;
+        cout << ">entrez le ligne :";
+        cin >> ligne;
+    } while (((colonne > ar.size()) || (ligne > ar[0].size())) || (ar[colonne-1][ligne-1] !=vide));
+    ar[ligne-1][colonne-1] = Joueur;
+}
+
+
 int main (){
+    bool tour = true;
     tik grille;
     initialise(grille);
-     grille [1][0] = X;
-     grille [1][1] = X;
+    grille[2][2] = O;
     print (grille);
+    joue(true, grille);
+    print(grille);
 
     return 0;
 }
